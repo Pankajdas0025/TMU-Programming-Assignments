2c55175737f9ff4ad1767a7ef30d53e89f4ed937
PANKAJ KUMAR DAS <pankajkumardas8678@gmail.com>
2025-09-03 23:36:11 +0530

    add

--- Java/JAVAASS1/Message.java
+++ Java/JAVAASS1/Message.java
@@ -1,7 +1,10 @@
 package mypackage;
 
-public class Message {
-    public void show() {
+public class Message
+
+{
+    public void show()
+    {
         System.out.println("Hello from the user-defined package!");
     }
 }
---
+++ Java/table.java
@@ -0,0 +1,21 @@
+
+
+
+public class table {
+    public static void main (String[] args)
+
+    {
+       int num = 5 ;
+
+       System.out.println( "Table of " +num);
+       for(int i = 1 ; i<=10 ; i++)
+       {
+        System.out.println( num+ "*"+i +" = " +num*i);
+       }
+
+    }
+
+
+    }
+
+
