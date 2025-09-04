30751a984c73b4e5fd359f52f537b0a328ba8ccd
PANKAJ KUMAR DAS <pankajkumardas8678@gmail.com>
2025-09-05 01:57:42 +0530

    day 1

---
+++ Java/Practice_Question/varargs.java
@@ -0,0 +1,29 @@
+public class varargs
+
+
+{
+public int Sum( int ...arr)
+        {
+
+int l = arr.length;
+int sum = 0 ;
+
+for(int x: arr)
+
+{
+    sum+=x;
+
+}
+return sum;
+        }
+
+    public static void main  (String [] args)
+
+    {
+
+   varargs obj = new varargs();
+
+   System.out.print("The Sum Of Numbers are ; " +obj.Sum(1100,2,3,4));
+    }
+
+}
