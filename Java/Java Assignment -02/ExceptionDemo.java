88b420e094deca54f68fe254561ed910ed2661b9
PANKAJ KUMAR DAS <pankajkumardas8678@gmail.com>
2025-09-19 00:00:07 +0530

    add

--- Java/Java Assignment -02/ExceptionDemo.java
+++ Java/Java Assignment -02/ExceptionDemo.java
@@ -5,13 +5,11 @@ class ExceptionDemo
     public static void main(String args[])
 
     {
-
         int a=10,b=0,c;
 
         try
 
         {
-
             c=a/b;
 
             System.out.println("Result = "+c);
